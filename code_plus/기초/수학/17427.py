8c3b41a19ba9e26007c181dcb16cfa6fefc04872
woolee <lebind12@naver.com>
2022-09-06 10:37:57 +0900

    [BOJ] 약수의 합 2 / Silver 2 / 30분

--- code_plus/기초/수학/17427.py
+++ code_plus/기초/수학/17427.py
@@ -1,6 +1,10 @@
 import sys
 import math
 
+"""
+- 직관적인 알고리즘
+- 시간초과에 걸림
+- math에 익숙해질 수 있었음.
 
 def get_divisor(num : int):
     divisor_list = []
@@ -25,3 +29,21 @@ def get_gvalue(num : int):
     return sum
 
 print(get_gvalue(1000000))
+"""
+
+"""
+입력값 num에 대하여
+g(num)을 계산할 때 f(1) 부터 f(num)까지 계산하면서
+2의 배수는 약수로 2를 항상 갖고, 3의 배수는 약수로 항상 3을 갖기 때문에
+g(num)을 계산할 때 2가 1부터 num까지의 수 중 2의 배수만큼 존재하게 됨.
+이를 이용하여 문제를 풀이
+"""
+
+def get_gvalue(num : int):
+    sum = 0 # 합을 저장할 변수
+    for i in range(1, num + 1):
+        sum += i * (num // i) # i는 num 에서 i를 나눈 몫만큼 존재
+    return (sum)
+
+num = int(sys.stdin.readline())
+print(get_gvalue(num))
